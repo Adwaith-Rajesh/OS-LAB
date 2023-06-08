afdadcf0522dca317e61a433b673b61650379e09
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-06-08 20:41:16 +0530

    Added exp 9 to python lab

---
+++ python-lab/exp-9.py
@@ -0,0 +1,25 @@
+# a.
+from scipy.stats import binom
+
+
+print('Exactly 4 success')
+print(binom.pmf(4, 6, 0.25))
+
+print('Exactly 1 success')
+y = 1 - binom.pmf(0, 6, 0.25)
+print(y)
+
+# output
+# Exactly 4 success
+# 0.03295898437499997
+# Exactly 1 success
+# 0.822021484375
+
+# b.
+from scipy.stats import poisson
+
+
+print(poisson.pmf(k=6, mu=3.4))
+
+# output
+# 0.07160440945982202
